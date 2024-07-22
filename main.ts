9be31288d6ea15be071de5b42154419cf92576c7
TurtleStoffel <me@turtlestoffel.com>
2024-07-22 20:51:57 +0200

    Run auto-formatting

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+    App,
+    Editor,
+    MarkdownView,
+    Modal,
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,8 +16,8 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-    mySetting: 'default'
-}
+    mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
     settings: MyPluginSettings;
@@ -19,54 +28,59 @@ export default class MyPlugin extends Plugin {
         this.registerEvent(
             this.app.workspace.on("editor-menu", (menu, editor, view) => {
                 menu.addItem((item) => {
-                    item
-                        .setTitle("Add to-do item")
-                        .onClick(async () => {
-                            new Notice(view.file?.path ?? "test");
-                            new CreateTodoItemModal(this.app, (result) => {
-                                new Notice(`Created a new item to-do item. ${result}`)
-                            }).open()
-                        });
+                    item.setTitle("Add to-do item").onClick(async () => {
+                        new Notice(view.file?.path ?? "test");
+                        new CreateTodoItemModal(this.app, (result) => {
+                            new Notice(
+                                `Created a new item to-do item. ${result}`
+                            );
+                        }).open();
+                    });
                 });
             })
         );
 
         // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-            // Called when the user clicks the icon.
-            new Notice('This is a notice!');
-        });
+        const ribbonIconEl = this.addRibbonIcon(
+            "dice",
+            "Sample Plugin",
+            (evt: MouseEvent) => {
+                // Called when the user clicks the icon.
+                new Notice("This is a notice!");
+            }
+        );
         // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
+        ribbonIconEl.addClass("my-plugin-ribbon-class");
 
         // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
         const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText('Status Bar Text');
+        statusBarItemEl.setText("Status Bar Text");
 
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open sample modal (simple)',
+            id: "open-sample-modal-simple",
+            name: "Open sample modal (simple)",
             callback: () => {
                 new SampleModal(this.app).open();
-            }
+            },
         });
         // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
+            id: "sample-editor-command",
+            name: "Sample editor command",
             editorCallback: (editor: Editor, view: MarkdownView) => {
                 console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
-            }
+                editor.replaceSelection("Sample Editor Command");
+            },
         });
         // This adds a complex command that can check whether the current state of the app allows execution of the command
         this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Open sample modal (complex)',
+            id: "open-sample-modal-complex",
+            name: "Open sample modal (complex)",
             checkCallback: (checking: boolean) => {
                 // Conditions to check
-                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                const markdownView =
+                    this.app.workspace.getActiveViewOfType(MarkdownView);
                 if (markdownView) {
                     // If checking is true, we're simply "checking" if the command can be run.
                     // If checking is false, then we want to actually perform the operation.
@@ -77,19 +91,21 @@ export default class MyPlugin extends Plugin {
                     // This command will only show up in Command Palette when the check function returns true
                     return true;
                 }
-            }
+            },
         });
 
         // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new SampleSettingTab(this.app, this));
     }
 
-    onunload() {
-
-    }
+    onunload() {}
 
     async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
     }
 
     async saveSettings() {
@@ -107,34 +123,31 @@ class CreateTodoItemModal extends Modal {
     }
 
     onOpen() {
-        const {contentEl} = this
+        const { contentEl } = this;
 
         contentEl.createEl("h1", { text: "Enter to-do item details" });
 
-        new Setting(contentEl)
-            .setName("description")
-            .addTextArea((text) => 
-                text.onChange((value) => {
-                    this.result = value
+        new Setting(contentEl).setName("description").addTextArea((text) =>
+            text.onChange((value) => {
+                this.result = value;
+            })
+        );
+
+        new Setting(contentEl).addButton((btn) =>
+            btn
+                .setButtonText("Submit")
+                .setCta()
+                .onClick(() => {
+                    this.close();
+                    this.onSubmit(this.result);
                 })
-            );
-        
-        new Setting(contentEl)
-            .addButton((btn) => 
-                btn
-                    .setButtonText("Submit")
-                    .setCta()
-                    .onClick(() => {
-                        this.close();
-                        this.onSubmit(this.result);
-                    })
-            );
+        );
     }
 
     onClose() {
-        const {contentEl} = this
+        const { contentEl } = this;
 
-        contentEl.empty()
+        contentEl.empty();
     }
 }
 
@@ -147,19 +160,21 @@ class SampleSettingTab extends PluginSettingTab {
     }
 
     display(): void {
-        const {containerEl} = this;
+        const { containerEl } = this;
 
         containerEl.empty();
 
         new Setting(containerEl)
-            .setName('Setting #1')
-            .setDesc('It\'s a secret')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
+            .setName("Setting #1")
+            .setDesc("It's a secret")
+            .addText((text) =>
+                text
+                    .setPlaceholder("Enter your secret")
+                    .setValue(this.plugin.settings.mySetting)
+                    .onChange(async (value) => {
+                        this.plugin.settings.mySetting = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
     }
 }
