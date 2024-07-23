1cfcc1d05406ba96bfa578d2254bbdf78638e1a7
TurtleStoffel <me@turtlestoffel.com>
2024-07-23 21:17:24 +0200

    Move CreateTodoItemModal to dedicated file

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import {
     App,
     Editor,
     MarkdownView,
-    Modal,
     Notice,
     Plugin,
     PluginSettingTab,
     Setting,
 } from "obsidian";
+import { CreateTodoItemModal } from "src/create-todo-item-modal";
 
 import { createTodo, getTodoItemsForFile } from "src/todo-manager";
 
@@ -128,44 +128,6 @@ export default class MyPlugin extends Plugin {
 
     async saveSettings() {
         await this.saveData(this.settings);
-    }
-}
-
-class CreateTodoItemModal extends Modal {
-    result: string;
-    onSubmit: (result: string) => void;
-
-    constructor(app: App, onSubmit: (result: string) => void) {
-        super(app);
-        this.onSubmit = onSubmit;
-    }
-
-    onOpen() {
-        const { contentEl } = this;
-
-        contentEl.createEl("h1", { text: "Enter to-do item details" });
-
-        new Setting(contentEl).setName("description").addTextArea((text) =>
-            text.onChange((value) => {
-                this.result = value;
-            })
-        );
-
-        new Setting(contentEl).addButton((btn) =>
-            btn
-                .setButtonText("Submit")
-                .setCta()
-                .onClick(() => {
-                    this.close();
-                    this.onSubmit(this.result);
-                })
-        );
-    }
-
-    onClose() {
-        const { contentEl } = this;
-
-        contentEl.empty();
     }
 }
 
