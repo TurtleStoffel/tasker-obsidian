052ad6c29d7cef3ca2e91d0e549cf8e788e30d62
TurtleStoffel <me@turtlestoffel.com>
2024-07-23 20:37:36 +0200

    Save to-do item to JSON file

--- src/todo-manager.ts
+++ src/todo-manager.ts
@@ -1,22 +1,28 @@
-import { Vault } from "obsidian";
+import { randomUUID } from "crypto";
+import { TFile, Vault } from "obsidian";
+
+const FILENAME = "test.json";
 
 export async function createTodo(vault: Vault, details: string) {
     console.log("Creating a new to-do item.");
 
-    const fileExists = await pluginFileExists(vault);
-    console.log(`Plugin file exists ${fileExists}`);
+    const pluginFile = await getOrCreatePluginFile(vault);
 
-    if (!fileExists) {
-        console.log("Creating new state file");
-        vault.create(getFilename(vault), "dummy");
+    vault.process(pluginFile, (data) => {
+        const parsedData = JSON.parse(data);
+        parsedData[randomUUID()] = details;
+        return JSON.stringify(parsedData);
+    });
+}
+
+async function getOrCreatePluginFile(vault: Vault): Promise<TFile> {
+    const file = vault.getFileByPath(FILENAME);
+
+    console.log(file);
+
+    if (file) {
+        return file;
+    } else {
+        return vault.create(FILENAME, JSON.stringify({}));
     }
-}
-
-function getFilename(vault: Vault) {
-    return `${vault.configDir}/test.json`;
-}
-
-async function pluginFileExists(vault: Vault) {
-    const result = await vault.adapter.stat(getFilename(vault));
-    return result != null;
 }
